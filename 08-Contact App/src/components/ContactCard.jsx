6ab49bf3f046cb4cd46d0225db57b46b9f9f182c
Ashutosh <vashutosh625@gmail.com>
2024-01-29 22:26:48 +0530

    updated contacts

--- 08-Contact App/src/components/ContactCard.jsx
+++ 08-Contact App/src/components/ContactCard.jsx
@@ -1,27 +1,51 @@
-import React, { useContext} from "react";
-import { IoIosContact } from "react-icons/io";
-import { MdOutlineEdit, MdDelete } from "react-icons/md";
-import { context } from "../context/AppContext";
+import { deleteDoc, doc } from "firebase/firestore";
+import { HiOutlineUserCircle } from "react-icons/hi";
+import { IoMdTrash } from "react-icons/io";
+import { RiEditCircleLine } from "react-icons/ri";
+import { db } from "../config/firebase";
+import AddAndUpdateContact from "./AddAndUpdateContact";
+import useDisclouse from "../hooks/useDisclouse";
+import { toast } from "react-toastify";
 
-const ContactCard = ({ data, onOpen }) => {
+const ContactCard = ({ contact }) => {
+  const { isOpen, onClose, onOpen } = useDisclouse();
 
-  const handleEdit= ()=>{
-    onOpen(data)
-  }
+  const deleteContact = async (id) => {
+    try {
+      await deleteDoc(doc(db, "contacts", id));
+      toast.success("Contact Deleted Successfully");
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <>
-    <div className="flex bg-[#FFEAAE] justify-evenly rounded-lg items-center p-2 gap-2">
-      <IoIosContact className="text-5xl text-yellow-800 w-[20%] flex-1" />
-      <div className="text-sm w-[50%]">
-        <p className="font-semibold">{data.name}</p>
-        <p className="">{data.email}</p>
-        <p className="">{data.mobileNumber}</p>
+      <div
+        key={contact.id}
+        className="flex items-center justify-between rounded-lg bg-yellow p-2"
+      >
+        <div className="flex gap-1">
+          <HiOutlineUserCircle className="text-4xl text-orange" />
+          <div className="">
+            <h2 className="font-medium">{contact.name}</h2>
+            <p className="text-sm">{contact.email}</p>
+          </div>
+        </div>
+        <div className="flex text-3xl">
+          <RiEditCircleLine onClick={onOpen} className="cursor-pointer" />
+          <IoMdTrash
+            onClick={() => deleteContact(contact.id)}
+            className="cursor-pointer text-orange"
+          />
+        </div>
       </div>
-      <MdOutlineEdit onClick={handleEdit} className="text-3xl text-gray-800 flex-1 w-[10%]" />
-      <MdDelete className="text-3xl text-purple-800 flex-1 w-[10%]" />
-    </div>
-   
+      <AddAndUpdateContact
+        contact={contact}
+        isUpdate
+        isOpen={isOpen}
+        onClose={onClose}
+      />
     </>
   );
 };
--- 08-Contact App/src/components/Modal.jsx
+++ 08-Contact App/src/components/Modal.jsx
@@ -1,17 +1,22 @@
-import React, { useRef, useContext, forwardRef, useState } from "react";
-import { context } from "../context/AppContext";
-import {collection} from "firebase/firestore"
-import {db} from '../config/firebase'
-const Modal = ({ onClose, isOpen }) => {
+import { createPortal } from "react-dom";
+import { AiOutlineClose } from "react-icons/ai";
 
-
-
-  return (
-    <div>
-      
-    </div>
-
-  )
+const Modal = ({ onClose, isOpen, children }) => {
+  return createPortal(
+    <>
+      {isOpen && (
+        <div className="absolute top-0 z-40 grid h-screen w-screen place-items-center backdrop-blur">
+          <div className="relative z-50 m-auto min-h-[200px] min-w-[80%] bg-white p-4">
+            <div className="flex justify-end">
+              <AiOutlineClose onClick={onClose} className="self-end text-2xl" />
+            </div>
+            {children}
+          </div>
+        </div>
+      )}
+    </>,
+    document.getElementById("modal-root")
+  );
 };
 
 export default Modal;
